896008607a7edf0a1a242542b84cdeb953fd21e5
unknown <okhaedokey1110@naver.com>
2020-07-13 13:59:04 +0900

    Updates

---
+++ precache-manifest.f78eb479fc4d6cb68779a55a53d236d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd5d8591b1b3289471c378d1f03a6ed0",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "76b217b6842d595fc1c7",
+    "url": "/movie_app_2019/static/css/main.18d0f143.chunk.css"
+  },
+  {
+    "revision": "118469322d9ff5a4cfd2",
+    "url": "/movie_app_2019/static/js/2.a786e030.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.a786e030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76b217b6842d595fc1c7",
+    "url": "/movie_app_2019/static/js/main.8656d95a.chunk.js"
+  },
+  {
+    "revision": "059a9ab2adfa282ae316",
+    "url": "/movie_app_2019/static/js/runtime-main.12568eff.js"
+  }
+]);
